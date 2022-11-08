714c830199f51a4f6f10c39dd8e78ce7f4416e58
arcetros <75669794+arcetros@users.noreply.github.com>
2022-11-08 14:43:55 +0800

    chore: move recipe hooks directly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,62 @@
 import type { GetStaticProps } from "next"
+import { useRouter } from "next/router"
 import React from "react"
 
 import { HomepageHeader, HomepageInfo } from "@/components/home"
 import HomepageRecipe from "@/components/home/homepage-recipe"
-import type { HomePageProps } from "@/components/home/types"
+import type { HomePageProps, QueryParam } from "@/components/home/types"
 import { Container } from "@/components/ui"
-import { useRecipe } from "@/libs/hooks"
+import { Result } from "@/components/ui/recipe/recipe"
+import { server } from "@/config"
+import { isValidHttpUrl } from "@/helpers/isValidHttp"
 
 export default function Home({ stats }: HomePageProps) {
-  const { recipeData, isRequested, handleSubmitForm, value, setValue, url } = useRecipe()
+  const router = useRouter()
+  const { url } = router.query as QueryParam
+
+  const [value, setValue] = React.useState<string>(url || "")
+  const [isRequested, setIsRequested] = React.useState<boolean>(false)
+  const [recipeData, setRecipeData] = React.useState<Result>()
+
+  async function fetchRecipe(targetUrl: string) {
+    setIsRequested(true)
+    try {
+      await fetch(`${server}/api/scrap`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(targetUrl)
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setRecipeData(data)
+          setIsRequested(false)
+        })
+    } catch (err) {
+      console.warn(err)
+    }
+  }
+
+  async function handleSubmitForm(event: React.FormEvent) {
+    event.preventDefault()
+    const query: { url: string } = { url: value }
+    if (value.length > 0 && isValidHttpUrl(query.url)) {
+      setRecipeData(undefined)
+      router.push({ pathname: "/", query: `url=${decodeURI(query.url)}` })
+      fetchRecipe(query.url)
+      setValue("")
+    } else {
+      alert("Make sure you entered valid url")
+    }
+  }
+
+  React.useEffect(() => {
+    if (url) {
+      fetchRecipe(url)
+    }
+  }, [url])
 
   return (
     <Container className="m-auto flex flex-col gap-y-8 divide-y">
