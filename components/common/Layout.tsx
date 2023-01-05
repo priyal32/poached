092b0313bd222d12226b53c2d150ae4b11d62a51
arcetros <75669794+arcetros@users.noreply.github.com>
2023-01-05 14:34:30 +0800

    feat: add landing page

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,49 +1,71 @@
-import React from "react"
-import { QueryClient, QueryClientProvider } from "react-query"
-import { ReactQueryDevtools } from "react-query/devtools"
+import clsx from "clsx";
+import { useRouter } from "next/router";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-import Navbar from "./Navbar/Navbar"
-import SEO from "./SEO"
+import Navbar from "./Navbar/Navbar";
+import SEO from "./SEO";
 
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const Layout: React.FunctionComponent<Props> = ({ children }) => {
-  const [isNavbarOpen, setIsNavbarOpen] = React.useState<boolean>(false)
-  const [clientWidth, setClientWidth] = React.useState<number | undefined>(undefined)
+  const router = useRouter();
 
-  const ref = React.useRef<HTMLDivElement>(null)
+  const [isNavbarOpen, setIsNavbarOpen] = React.useState<boolean>(false);
+  const [clientWidth, setClientWidth] = React.useState<number | undefined>(undefined);
 
-  const toggleNavBar = () => setIsNavbarOpen((state) => !state)
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  const toggleNavBar = () => setIsNavbarOpen((state) => !state);
+  const isHome = router.pathname.includes("/home");
 
   React.useEffect(() => {
     function handleResize() {
-      setClientWidth(ref.current?.offsetWidth)
+      setClientWidth(ref.current?.offsetWidth);
     }
-    window.addEventListener("resize", handleResize)
+    window.addEventListener("resize", handleResize);
 
     if (clientWidth && clientWidth > 768) {
-      setIsNavbarOpen(false)
+      setIsNavbarOpen(false);
     }
 
-    return () => window.removeEventListener("resize", handleResize)
-  }, [ref.current?.offsetWidth])
+    return () => window.removeEventListener("resize", handleResize);
+  }, [ref.current?.offsetWidth]);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <main ref={ref} className="flex h-auto flex-col overflow-hidden md:h-screen md:flex-row">
+      <main ref={ref} className={clsx("flex h-auto md:h-screen", isHome ? "flex-col" : "flex-col overflow-hidden md:flex-row")}>
         <SEO>
           <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1" />
         </SEO>
-        <Navbar isOpen={isNavbarOpen} toggleNavBar={toggleNavBar} />
-        <div className="h-screen w-full overflow-auto pt-[80px] md:pl-[15rem] md:pt-0">{children}</div>
+        {!isHome ? (
+          <React.Fragment>
+            <Navbar isOpen={isNavbarOpen} toggleNavBar={toggleNavBar} />
+            <div className="h-screen w-full overflow-auto pt-[80px] md:pl-[15rem] md:pt-0">{children}</div>
+          </React.Fragment>
+        ) : (
+          <section className="pb-8">
+            <div className="sticky top-0 z-50 w-full bg-transparent">
+              <div className="pointer-events-none absolute z-[-1] h-full w-full bg-dark-2 bg-opacity-80 shadow-[0_-1px_0_rgba(255,255,255,.1)_inset] contrast-[0_0_0_1px_#fff] backdrop-blur-md"></div>
+              <nav className="flex h-16 items-center justify-between px-8">
+                <div className="flex items-center">
+                  <h1 className="font-primary text-2xl font-medium">Poached</h1>
+                  <span className="ml-1 rounded bg-dark-1 py-1 px-2 text-xs font-medium">BETA</span>
+                </div>
+              </nav>
+            </div>
+            <section className="mx-auto w-full max-w-5xl">{children}</section>
+          </section>
+        )}
       </main>
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
---
+++ pages/home.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import React from "react";
+import { BsFillArrowRightCircleFill, BsGlobe } from "react-icons/bs";
+import { HiBookOpen, HiCloud, HiLibrary } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div className="flex w-full flex-col items-center">
+      <section className="px-6 md:px-0">
+        <div className="gradient-02 absolute inset-0 -z-50 h-[50%] w-[30%] rounded-full opacity-40"></div>
+        <div className="mx-auto max-w-4xl pt-28 md:pt-36">
+          <h2 className="text-center font-headline text-lg font-bold text-[hsl(144,42%,47%)]">The home for all your recipes</h2>
+          <h1 className="text-center font-headline text-4xl font-bold md:text-7xl">All your recipes in one single page.</h1>
+          <p className="mx-auto mt-8 max-w-3xl text-center font-headline text-base md:mt-12 md:text-xl">
+            Import just the recipe from any website without the distractions or clutter. Create meal plans, and generate grocery lists
+          </p>
+          <div className="mt-12 flex items-center justify-center gap-x-4">
+            <button className="flex h-12 items-center gap-x-1.5 rounded-lg bg-[hsl(144,42%,47%)] px-2 md:px-4">
+              <span className="text-sm font-bold md:text-lg">Create an Account</span>
+              <BsFillArrowRightCircleFill className="h-5 w-5" />
+            </button>
+            <button className="flex h-12 items-center gap-x-1.5 rounded-lg px-2 outline outline-dark-1 md:px-4">
+              <span className="text-sm font-bold md:text-lg">Explore</span>
+              <BsGlobe className="h-5 w-5" />
+            </button>
+          </div>
+        </div>
+      </section>
+      <div className="py-24 px-6 md:px-0">
+        <Image alt="mock" width={1200} height={700} src="/mock.png" className="rounded-xl" />
+      </div>
+      <section className="w-full px-6 md:px-0">
+        <div className="gradient-01 absolute right-0 -z-50 h-[50%] w-[30%] rounded-full opacity-40"></div>
+        <ul className="grid w-full grid-cols-1 gap-8 md:grid-cols-3">
+          <li>
+            <HiBookOpen className="h-10 w-10" />
+            <h5 className="mt-2 text-xl font-medium">Make recipes your own</h5>
+            <p className="mt-4 text-neutral-400">You can easily edit recipes, save adjustments to ingredients, and add additional steps or tips to your preparation.</p>
+          </li>
+          <li>
+            <HiLibrary className="h-10 w-10" />
+            <h5 className="mt-2 text-xl font-medium">All in one place</h5>
+            <p className="mt-4 text-neutral-400">Storing your recipes in Poached allows you to quickly search, find, and select what you want to cook.</p>
+          </li>
+          <li>
+            <HiCloud className="h-10 w-10" />
+            <h5 className="mt-2 text-xl font-medium">Cook from your favorite device</h5>
+            <p className="mt-4 text-neutral-400">Poached stores your recipes in the Cloud so you can access them on any device through our website or Android/iOS app.</p>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
