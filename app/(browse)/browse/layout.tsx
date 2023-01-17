5889c0d29d96172fbbed6b3d1e735829624eaebd
arcetros <75669794+arcetros@users.noreply.github.com>
2023-01-18 00:28:49 +0800

    forgot to add clientWidth listener hook

--- app/(browse)/browse/layout.tsx
+++ app/(browse)/browse/layout.tsx
@@ -5,14 +5,30 @@ import React from "react";
 
 const BrowseLayout = ({ children }: React.PropsWithChildren<unknown>) => {
   const [isNavbarOpen, setIsNavbarOpen] = React.useState<boolean>(false);
+  const [clientWidth, setClientWidth] = React.useState<number | undefined>(undefined);
+
+  const ref = React.useRef<HTMLDivElement>(null);
 
   const toggleNavBar = () => setIsNavbarOpen((state) => !state);
 
+  React.useEffect(() => {
+    function handleResize() {
+      setClientWidth(ref.current?.offsetWidth);
+    }
+    window.addEventListener("resize", handleResize);
+
+    if (clientWidth && clientWidth > 768) {
+      setIsNavbarOpen(false);
+    }
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [ref.current?.offsetWidth]);
+
   return (
-    <React.Fragment>
+    <main ref={ref}>
       <Navbar isOpen={isNavbarOpen} toggleNavBar={toggleNavBar} />
       {children}
-    </React.Fragment>
+    </main>
   );
 };
 
