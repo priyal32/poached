9d57c35efa616435d9a2751a1e8916aed830bd70
arcetros <75669794+arcetros@users.noreply.github.com>
2023-01-26 18:00:05 +0800

    feat(auth): add head for signin & signup

---
+++ app/(auth)/signin/head.tsx
@@ -0,0 +1,8 @@
+import PageSeo from "@components/page-seo";
+import React from "react";
+
+const SignInHead = () => {
+  return <PageSeo title="Sign In" />;
+};
+
+export default SignInHead;
---
+++ app/(auth)/signup/head.tsx
@@ -0,0 +1,8 @@
+import PageSeo from "@components/page-seo";
+import React from "react";
+
+const SignUpHead = () => {
+  return <PageSeo title="Sign Up" />;
+};
+
+export default SignUpHead;
